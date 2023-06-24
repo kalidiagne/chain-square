733e922889d0277ee66da09f4506bcb81620e201
Julien Genestoux <julien.genestoux@gmail.com>
2023-06-24 11:00:21 -0400

    adding prisma setup

---
+++ migrations/20230624141259_/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Scores" (
+    "id" SERIAL NOT NULL,
+    "userAddress" TEXT NOT NULL,
+    "criterion" TEXT NOT NULL,
+    "score" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Scores_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Scores_userAddress_idx" ON "Scores"("userAddress");
+
+-- CreateIndex
+CREATE INDEX "Scores_criterion_idx" ON "Scores"("criterion");
+
+-- CreateIndex
+CREATE INDEX "Scores_score_idx" ON "Scores"("score");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Scores_criterion_userAddress_key" ON "Scores"("criterion", "userAddress");
